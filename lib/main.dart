615f037b70ab094437a861c1f72b90363f28b3a2
jordan-julio <51514356+jordan-julio@users.noreply.github.com>
2020-08-15 22:37:10 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'home.dart';
-import 'login.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +9,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // return FutureBuilder<FirebaseUser>(
+    //     future: FirebaseAuth.instance.currentUser(),
+    //     builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
+    //       if (snapshot.hasData) {
+    //         FirebaseUser user = snapshot.data; // this is your user instance
+    //         /// is because there is user already logged
     return Home();
+    //       }
+    /// other way there is no user logged.
+    // return Login();
+    // });
   }
 }
